24d70a6122742e32b1c0975669bfd6839be461ba
Billy Martin <billy@sitesculptors.com>
2023-06-08 12:07:53 -0400

    Fixed Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Footer from './Views/Footer'
 import Header from './Views/Header'
-import Main from './Views/Main'
+import About from './Views/About'
+import Contact from './Views/Contact'
+import Home from './Views/Home'
+import Workouts from './Views/Workouts'
+import Recipes from './Views/Recipes'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <Main />
-    <Footer />     
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Recipes" element={<Recipes />} />
+          <Route path="/Workouts" element={<Workouts />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Views/Recipes.tsx
@@ -0,0 +1,13 @@
+
+export default function Recipes() {
+
+  return (
+    <div className="container">
+      <div className="row row align-items-center mt-5"  >
+        <div className="col-6">
+          <h1 className="m-4 align-middle b" >Recipes</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
