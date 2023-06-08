5a87ae6768abecdaa20cfc9f127cd7a5c6c63f45
Billy Martin <billy@sitesculptors.com>
2023-06-07 20:30:39 -0400

    Moved code to Header, Main, and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
+
 import './App.css'
-import MainHeader from './Views/MainHeader.tsx'
-import NavBar from './Views/NavBar.tsx'
+import Footer from './Views/Footer'
+import Header from './Views/Header'
+import Main from './Views/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <NavBar />
-    <MainHeader />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Created by Scott Garber and Billy Martin
-      </p>
+    <Header />
+    <Main />
+    <Footer />     
     </>
   )
 }
---
+++ src/Views/Components/LoginForm.tsx
@@ -0,0 +1,15 @@
+
+function LoginForm() {
+
+  return(
+<div className="container">
+      <div className="row row align-items-center"  >
+      <div className="col-6">
+      <h1 className="m-4 align-middle b" >Login</h1>
+      </div>
+     
+      </div>
+      </div>
+)}
+
+export default LoginForm
