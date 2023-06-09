ccd34c47f180e30db00da4b356ba445514d9c135
Billy Martin <billy@sitesculptors.com>
2023-06-08 20:15:15 -0400

    Added Hamburger button

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,13 +4,18 @@ export default function NavBar() {
     const isActive = (path: string) => window.location.pathname === path;
 
     return (
-        <nav className='navbar fixed-top navbar-expand-lg justify-content-end px-5'>
-            <NavLink to="/" className={`nav-item nav-link mx-4 ${isActive('/') ? 'active' : ''}`}>Home</NavLink>
-            <NavLink to="/recipes" className={`nav-item nav-link mx-4 ${isActive('/recipes') ? 'active' : ''}`}>Recipes</NavLink>
-            <NavLink to="/workouts" className={`nav-item nav-link mx-4 ${isActive('/workouts') ? 'active' : ''}`}>Workouts</NavLink>
-            <NavLink to="/about" className={`nav-item nav-link mx-4 ${isActive('/about') ? 'active' : ''}`}>About</NavLink>
-            <NavLink to="/contact" className={`nav-item nav-link mx-4 ${isActive('/contact') ? 'active' : ''}`}>Contact</NavLink>
-            <NavLink to="/signin" className={`nav-item nav-link me-2 ms-5 ${isActive('/signin') ? 'active' : ''}`}>Sign In</NavLink>
+        <nav className='navbar fixed-top navbar-expand-md'>
+            <button className="navbar-toggler ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end px-5" id="navbarTogglerDemo03">
+                <NavLink to="/" className={`nav-item nav-link mx-4 ${isActive('/') ? 'active' : ''}`}>Home</NavLink>
+                <NavLink to="/recipes" className={`nav-item nav-link mx-4 ${isActive('/recipes') ? 'active' : ''}`}>Recipes</NavLink>
+                <NavLink to="/workouts" className={`nav-item nav-link mx-4 ${isActive('/workouts') ? 'active' : ''}`}>Workouts</NavLink>
+                <NavLink to="/about" className={`nav-item nav-link mx-4 ${isActive('/about') ? 'active' : ''}`}>About</NavLink>
+                <NavLink to="/contact" className={`nav-item nav-link mx-4 ${isActive('/contact') ? 'active' : ''}`}>Contact</NavLink>
+                <NavLink to="/signin" className={`nav-item nav-link me-2 ms-5 ${isActive('/signin') ? 'active' : ''}`}>Sign In</NavLink>
+            </div>
         </nav>
     );
 }
