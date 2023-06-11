654bca30dccd14f966d7dfa4b74f193887d828f5
Billy Martin <billy@sitesculptors.com>
2023-06-11 10:57:25 -0400

    Created ListGroup

--- src/Pages/Recipes.tsx
+++ src/Pages/Recipes.tsx
@@ -1,8 +1,18 @@
+import ListGroup from "../Components/ListGroup.tsx";
+
 export default function Recipes() {
+
+  const appetizers = ["Gluten-Free CheeseCake", "Lowfat Mousse", "Strawberry Smoothies"];
+  const desserts = ["Gluten-Free CheeseCake", "Lowfat Mousse", "Strawberry Smoothies"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
   return (
     <div className="row row align-items-center mt-5">
       <div className="col-6">
         <h1 className="m-4 align-middle b">Healthier Recipes</h1>
+        <ListGroup items={appetizers} heading="Appetizers" onSelectItem={handleSelectItem} />
+        <ListGroup items={desserts} heading="Desserts" onSelectItem={handleSelectItem} />
       </div>
     </div>
   );
