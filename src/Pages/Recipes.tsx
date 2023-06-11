baa018e071df27f0865c59cf17e3b224c23ab4a8
Billy Martin <billy@sitesculptors.com>
2023-06-11 11:37:32 -0400

    Added State Change

--- src/Pages/Recipes.tsx
+++ src/Pages/Recipes.tsx
@@ -1,32 +1,37 @@
 import ListGroup from "../Components/ListGroup.tsx";
+import { useState } from "react";
 
 export default function Recipes() {
 
-  const appetizers = ["Gluten-Free CheeseCake", "Lowfat Mousse", "Strawberry Smoothies"];
+  const appetizers = ["Fat-Free Queso", "Shrimp Cocktail", "Low-Fat Mozzerella Sticks"];
   const desserts = ["Gluten-Free CheeseCake", "Lowfat Mousse", "Strawberry Smoothies"];
-  let selectedAppetizer = "";
-  let selectedDessert = "";
+
+  const [selectedAppetizer, setSelectedAppetizer] = useState("");
+  const [selectedDessert, setSelectedDessert] = useState("");
   const handleSelectedAppetizer = (item: string) => {
-    selectedAppetizer = item;
+    console.log(item);
+
+    setSelectedAppetizer(item);
   }
   const handleSelectedDessert = (item: string) => {
-    selectedDessert = item;
+    console.log(item);
+    setSelectedDessert(item);
   }
   return (
     <div className="row row align-items-center mt-5">
       <div className="col-6">
         <h1 className="m-4 align-middle b">Healthier Recipes</h1>
         <div className="row">
-          <div className="col-8">
+          <div className="col-7">
             <ListGroup items={appetizers} heading="Appetizers" onSelectItem={handleSelectedAppetizer} />
           </div>
-          <h5 className="col-4 align-center">{selectedAppetizer}</h5>
+          <h5 className="col-5 align-center">{selectedAppetizer}</h5>
         </div>
         <div className="row">
-          <div className="col-8">
+          <div className="col-7">
             <ListGroup items={desserts} heading="Desserts" onSelectItem={handleSelectedDessert} />
           </div>
-          <h5 className="col-4 align-center">{selectedDessert}</h5>
+          <h5 className="col-5 align-center">{selectedDessert}</h5>
         </div>
       </div>
     </div>
