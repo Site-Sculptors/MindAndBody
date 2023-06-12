e017c3dd3aa8d96b2de6615ea143dbe0d346dd9d
Billy Martin <billy@sitesculptors.com>
2023-06-11 20:02:06 -0400

    Handles form refs

--- src/Components/SigninForm.tsx
+++ src/Components/SigninForm.tsx
@@ -1,15 +1,25 @@
-import { Col, Form, Row, Stack } from "react-bootstrap";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react"
 
 const SigninForm = () => {
+    const emailRef = useRef<HTMLInputElement>(null)
+    const passwordRef = useRef<HTMLInputElement>(null)
+
     const [passwordVisible, setPasswordVisibility] = useState(false);
-    const handleSigninSubmit = () => {
-        console.log("Signed In!");
+
+    const handleSigninSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log("Signed In: ");
+        console.log(emailRef.current!.value);
+        console.log(passwordRef.current!.value);
+
     }
     const handlePasswordVisibilty = () => {
         console.log(passwordVisible);
         setPasswordVisibility(!passwordVisible)
+
     }
     return (
         <Form onSubmit={handleSigninSubmit}>
@@ -18,18 +28,18 @@ const SigninForm = () => {
                     <Col>
                         <Form.Group controlId="email" >
                             <Form.Label>Email Address</Form.Label>
-                            <Form.Control required type="email" />
+                            <Form.Control type="email" ref={emailRef} required />
                         </Form.Group>
                         <Form.Group controlId="password" >
                             <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" />
+                            <Form.Control required type="password" ref={passwordRef} />
                             <button onClick={() => { handlePasswordVisibilty }} className={passwordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'}></button>
 
                         </Form.Group>
                         <Stack direction="horizontal" className="justify-content-center mx-4" gap={5} >
-                            <button type="submit" className="btn btn-success" >Sign In</button>
+                            <Button type="submit" className="btn btn-success" >Sign In</Button>
                             <NavLink to="/" className={`nav-item nav-link`}>
-                                <button type="button" className="btn btn-danger"  >Cancel</button>
+                                <Button type="button" className="btn btn-danger"  >Cancel</Button>
                             </NavLink>
 
                         </Stack>
